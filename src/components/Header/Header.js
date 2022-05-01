4e7519bbd8a8f8b5bd74f24e86c97930edda82c6
Datsede04 <Datsede04@gmail.com>
2022-05-02 00:40:32 +0300

    Header component is added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react"
+import './Header.scss'
+import WhiteLogo from '../../assets/img/logo-white.png'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header__logo-box">
+            <img src={WhiteLogo} alt="log" className="header__logo"/>
+        </div>
+
+        <div className="header__text-box">
+            <h1 className="headding-primary">
+                <span className="headding-primary--main">Outdoors</span>
+                <span className="headding-primary--sub">is where life happens</span>
+            </h1>
+
+            <a href="#" className="btn btn--white btn--animated">discover our tour</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
